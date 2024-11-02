b951612e299e7d166b9613fafd3017472af0c20d
NitroGuy10 <naviyer10@gmail.com>
2024-11-02 13:56:31 -0500

    LETS GOO

---
+++ backend/express/app.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.disable('etag');
+const PORT = 4000;
+
+let game = {
+  player1: {
+    "segments": {
+        "x": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "y": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+  },
+  
+  player2: {
+    "segments": {
+        "x": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "y": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+  },
+  
+  ai: {
+    "segments": {
+        "x": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "y": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+  }
+}
+
+
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/static')));
+
+app.get('/host', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'host.html'));
+});
+
+app.get('/player1', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'player.html'));
+});
+
+app.get('/player2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'player.html'));
+});
+
+// POST route for reporting player segments
+app.post('/report-segments', (req, res) => {
+  const data = req.body;
+  if (data.player === 'player1') {
+    game.player1.segments.x = data.x;
+    game.player1.segments.y = data.y;
+  } else if (data.player === 'player2') {
+    game.player2.segments.x = data.x;
+    game.player2.segments.y = data.y;
+  }
+  console.log(game.player1);
+
+  res.send('OK');
+});
+
+// GET route to retrieve segments
+app.get('/get-segments', (req, res) => {
+  const player = req.query.player;
+  const returnData = {
+    ai: game.ai.segments,
+  };
+
+  if (player === 'player1') {
+    returnData.player = game.player2.segments;
+  } else if (player === 'player2') {
+    returnData.player = game.player1.segments;
+  }
+
+  // console.log(returnData);
+  res.json(returnData);
+});
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('<p>Hello, World!</p>');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
